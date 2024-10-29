c28e6125fafff95e448a6247e3e2f65daffbd8cb
Areeb ur Rub <areeburrub@gmail.com>
2024-10-30 01:44:42 +0530

    fix: Date Format

--- src/components/ClientDateTimeDisplay.tsx
+++ src/components/ClientDateTimeDisplay.tsx
@@ -4,6 +4,6 @@ import {format} from "date-fns";
 export default function DateDisplay({ datetime }: { datetime: Date}) {
 
     return (
-        <span>{format(new Date(datetime), "dd MMM yyyy - HH:MM aa")}</span>
+        <span>{format(new Date(datetime), "dd MMM yyyy - hh:mm aa")}</span>
     );
 }
